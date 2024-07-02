eb662652837f126527d31626fa09a6a4e7f719fe
Vladislav Flinski <flinssski@gmail.com>
2024-07-02 15:20:10 +0300

    feat: add stars bg

--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
 'use strict'
+
+function createStars() {
+	const stars = document.getElementById('stars')
+	const numStars = 1000
+
+	for (let i = 0; i < numStars; i++) {
+		const star = document.createElement('div')
+		const starSize = Math.random() * 2
+
+		star.classList.add('star')
+
+		star.style.width = `${starSize}px`
+		star.style.top = `${Math.random() * 100}%`
+		star.style.left = `${Math.random() * 100}%`
+		star.style.animationDelay = `${Math.random() * 3}s`
+
+		stars.append(star)
+	}
+}
+
+document.addEventListener('DOMContentLoaded', createStars)
