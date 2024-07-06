42f71dc3df68c4ccd4da307fe5cf3229e38878eb
Vladislav Flinski <flinssski@gmail.com>
2024-07-06 12:40:42 +0300

    feat: add linking

--- src/script.js
+++ src/script.js
@@ -1,1 +1,32 @@
 'use strict'
+
+const navButtons = document.querySelectorAll('.header__button')
+const header = document.querySelector('.header')
+
+function getCoords(elem) {
+	let rect = elem.getBoundingClientRect()
+
+	return {
+		top: rect.top + window.scrollY,
+		right: rect.right + window.scrollX,
+		bottom: rect.bottom + window.scrollY,
+		left: rect.left + window.scrollX,
+	}
+}
+
+navButtons.forEach((navButton) => {
+	const id = navButton.getAttribute('href').slice(1)
+
+	navButton.addEventListener('click', (e) => {
+		e.preventDefault()
+
+		const section = document.getElementById(id)
+		const coords = getCoords(section)
+
+		window.scrollTo({
+			top: coords.top - header.offsetHeight,
+			left: 0,
+			behavior: 'smooth',
+		})
+	})
+})
